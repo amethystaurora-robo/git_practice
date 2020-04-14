81ccff0c0e14b716fdb3b6323e351ae0f280bd17
Amethyste <ameest95@gmail.com>
2020-04-14 21:59:20 +0700

    Exercise 01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,5 @@
 package labs_examples.arrays.labs;
-
+import java.util.Scanner;
 /**
  * Arrays calculator
  *
@@ -11,5 +11,23 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in 10 numbers.");
+        //int[] numList = scanner.nextInt();
+        int sum = 0;
+        int[] numList = {0, 5, 3, 7, 9, 10, 8, 7, 6, 5};
+        int j = 0;
+
+        for (int i = 0; i < numList.length; i++) {
+            sum = numList[i] + j;
+            j = numList[i] + j;
+        }
+        int average = sum/10;
+        System.out.println("The average is " + average);
+        System.out.println("The sum is " + sum);
+
+
+    }
 
 }
