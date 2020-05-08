6caac16b061de3ce7aab1b1e889586392307af07
Amethyste <ameest95@gmail.com>
2020-05-08 21:43:57 +0700

    Interface Example

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Dogses.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Dogses.java
@@ -6,14 +6,35 @@ class implementDogses {
         Bentley bentley = new Bentley();
         UnknownDog unknownDog = new UnknownDog();
 
+        runDogses dogses = new runDogses();
+
+        baxter.sit();
+        baxter.bark();
+        baxter.beQuiet();
+
     }
 }
 
 public interface Dogses {
-    public boolean bark(boolean isThundering, boolean strangerAtTheDoor);
+    default boolean bark(boolean isThundering, boolean strangerAtTheDoor) {
+        boolean barking = true;
+
+        if (isThundering == true || strangerAtTheDoor == true) {
+            barking = true;
+        }
+        else {
+            barking = false;
+        }
+        return barking;
+    }
+
+    default boolean bark() {
+        boolean barking;
+        barking = true;
+        return barking;
+    }
     public void beQuiet();
-    public void come();
-    public void sit();
+    public boolean sit();
 }
 
 class Baxter implements Dogses {
@@ -30,45 +51,115 @@ class Baxter implements Dogses {
         return barking;
     }
 
-
+    boolean barking;
     @Override
-    public void beQuiet() {}
+    public void beQuiet() {
+        boolean beQuiet;
+        if (barking == false) {
+            beQuiet = true;
+        }
+        else {
+            beQuiet = false;
+        }
+    }
 
+    boolean foodIsOffered;
+    boolean sitting;
     @Override
-    public void come() {}
-
-    @Override
-    public void sit() {}
+    public boolean sit() {
+        if (foodIsOffered == true) {
+            sitting = true;
+        }
+        else {
+            sitting = false;
+        }
+        return sitting;
+    }
 
 
 }
 
 class Bentley implements Dogses {
+    boolean barking;
     @Override
     public boolean bark(boolean toyWasStolen, boolean strangerAtTheDoor) {
+        if (toyWasStolen == true || strangerAtTheDoor == true) {
+            barking = true;
+        }
+        else {
+            barking = false;
+        }
+        return barking;
 
     }
 
     @Override
-    public void beQuiet() {}
+    public void beQuiet() {
+        boolean beQuiet;
+        if (barking == false) {
+            beQuiet = true;
+        }
+        else {
+            beQuiet = false;
+        }
+    }
 
+    boolean foodIsOffered;
+    boolean sitting;
     @Override
-    public void come() {}
-
-    @Override
-    public void sit() {}
+    public boolean sit() {
+        if (foodIsOffered == true) {
+            sitting = true;
+        }
+        else {
+            sitting = false;
+        }
+        return sitting;
+    }
 }
 
 class UnknownDog implements Dogses {
-    @Override
-    public boolean bark() {}
 
     @Override
-    public void beQuiet() {}
+    public boolean bark() {
+        boolean barking;
+        barking = true;
+        return barking;
+    }
 
+    boolean barking;
     @Override
-    public void come() {}
+    public void beQuiet() {
+        boolean beQuiet;
+        if (barking == false) {
+            beQuiet = true;
+        }
+        else {
+            beQuiet = false;
+        }
+    }
 
+    boolean foodIsOffered;
+    boolean sitting;
     @Override
-    public void sit() {}
+    public boolean sit() {
+       sitting = false;
+       return sitting;
+    }
 }
+
+class runDogses {
+
+    Dogses dogses;
+
+    public void setDogses() {
+        this.dogses = dogses;
+    }
+
+    public void testDogses() {
+        dogses.bark();
+        dogses.beQuiet();
+        dogses.sit();
+    }
+}
+
