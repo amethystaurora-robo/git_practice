c9ae7477ff331e76800058861f29d8d435efe3de
Amethyste <ameest95@gmail.com>
2020-05-13 17:04:22 +0700

    Package Example Two

---
+++ src/labs_examplest02/packages/labs/Exampletwo.java
@@ -0,0 +1,11 @@
+package labs_examplest02.packages.labs;
+
+public class Exampletwo {
+    public boolean returnTrue() {
+        return true;
+    }
+
+    protected boolean returnFalse() {
+        return false;
+    }
+}
