3aedc015b4a78e4a092b299589af091025340cc6
Amethyste <ameest95@gmail.com>
2020-05-13 17:04:12 +0700

    Package Example One

---
+++ src/labs_examples01/packages/labs/Example.java
@@ -0,0 +1,25 @@
+package labs_examples01.packages.labs;
+import labs_examplest02.packages.labs.*;
+
+public class Example {
+    public void addNum(int a, int b) {
+        int c = a + b;
+    }
+
+    protected int subNum(int a, int b) {
+        int c = a-b;
+        return c;
+    }
+    public static void main(String[] args) {
+        Exampletwo exampletwo = new Exampletwo();
+
+        //this works because it is public
+        exampletwo.returnTrue();
+
+        //this doesn't work because it's protected
+        exampletwo.returnFalse();
+
+    }
+
+
+}
