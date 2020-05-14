14afe9a3e4c9bb5fa7892e96ebfbe3719ed68c5c
Amethyste <ameest95@gmail.com>
2020-05-14 13:06:21 +0700

    Exception Practice 07

--- src/labs_examples/exception_handling/labs/Exercise_07.java
+++ src/labs_examples/exception_handling/labs/Exercise_07.java
@@ -1,4 +1,5 @@
 package labs_examples.exception_handling.labs;
+import java.util.*;
 
 /**
  * Exception Handling Exercise 7:
@@ -6,3 +7,26 @@ package labs_examples.exception_handling.labs;
  *      1) Create a custom exception.
  *      2) Demonstrate a method throwing your custom exception.
  */
+
+class notMineException extends Exception {
+    public void notMine() {
+        System.out.println("this is not mine. Take it.");
+    }
+}
+
+class customExceptionExample {
+    public static void main(String[] args) throws notMineException {
+        boolean mine;
+        Scanner scanner = new Scanner(System.in);
+        String userAnswer = scanner.nextLine();
+        System.out.println("Is it yours?");
+        if(userAnswer.equalsIgnoreCase("yes")) {
+            throw new notMineException();
+        }
+        else {
+            System.out.println("Give it back.");
+        }
+
+
+    }
+}
