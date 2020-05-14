31fb6676742ee97641fa472727d7f3355c899d30
Amethyste <ameest95@gmail.com>
2020-05-14 13:05:04 +0700

    Exception Practice 02

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -8,3 +8,15 @@ package labs_examples.exception_handling.labs;
  */
 
 
+class manyCatch {
+    public static void main(String[] args) {
+        int[] numList = {3, 0, 5, 7, 9};
+        try {
+            System.out.println(numList[10]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Oops. There aren't that many numbers in the list.");
+        } catch (Exception f) {
+            System.out.println("A problem has occurred.");
+        }
+    }
+}
