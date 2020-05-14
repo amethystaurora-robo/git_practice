1437fa5ad787588917371c15ba70794233ccbd5b
Amethyste <ameest95@gmail.com>
2020-05-14 13:05:29 +0700

    Exception Practice 04

--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,18 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+class nestedTryCatch {
+    public static void main(String[] args) {
+        int[] numList = {3, 0, 3, 5};
+        try {
+            System.out.println(numList[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("There aren't that many numbers in the list");
+        } try {
+            System.out.println(numList[3]);
+        } catch (ArrayIndexOutOfBoundsException f) {
+            System.out.println("Try again");
+        }
+    }
+}
