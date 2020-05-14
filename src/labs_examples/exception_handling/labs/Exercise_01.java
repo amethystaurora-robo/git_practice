a55c1cf4430102f074740a34f74b000ce0d5867b
Amethyste <ameest95@gmail.com>
2020-05-14 13:04:51 +0700

    Exception Practice 01

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -7,3 +7,15 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class tryCatchExample {
+    public static void main(String[] args) {
+        int[] numList = {3, 5, 7, 9, 0};
+        try {
+            int c = numList[1]/numList[4];
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+    }
+
+}
+
