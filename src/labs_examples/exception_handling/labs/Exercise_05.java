61932e204e412b45404aae519432bd577351bab6
Amethyste <ameest95@gmail.com>
2020-05-14 13:05:45 +0700

    Exception Practice 05

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -6,3 +6,15 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate how to throw an exception.
  *
  */
+
+class throwExample {
+    public static void main(String[] args) {
+        int[] numList = {0, 0, 0, 0};
+        int i = 0;
+            System.out.println(numList[i]);
+            if (numList[i] > numList.length) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+
+    }
+}
