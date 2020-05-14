217f01442e614e7d2ffa8899771250c9ae9cf8e9
Amethyste <ameest95@gmail.com>
2020-05-14 13:05:16 +0700

    Exception Practice 03

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,16 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class tryCatchFinally {
+    public static void main(String[] args) {
+        int[] numList = {3, 0, 5, 7};
+        try {
+            System.out.println(numList[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("There aren't that many numbers in the array");
+        } finally {
+            System.out.println("You did it.");
+        }
+    }
+}
