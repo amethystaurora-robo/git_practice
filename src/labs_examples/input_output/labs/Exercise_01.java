b63f7f1ae80a476ee1991d3261183972e3ee83f6
Amethyste <ameest95@gmail.com>
2020-05-21 14:31:20 +0700

    Exercise 01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,5 @@
 package labs_examples.input_output.labs;
-
+import java.io.*;
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -11,6 +11,29 @@ package labs_examples.input_output.labs;
 
 class Example {
     public static void main(String[] args) {
+        String filePath = "src/labs_examples/input_output/files/byte_data";
+        String writeFile = "src/labs_examples/input_output/files/bufferData.txt";
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(filePath));
+            byte[] buffer = new byte[5];
+            bis.read(buffer);
+            bos = new BufferedOutputStream(new FileOutputStream(writeFile));
+            bos.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bis.close();
+                bos.close();
+            } catch (IOException f) {
+                f.printStackTrace();
+            }
+        }
+
 
 
 
