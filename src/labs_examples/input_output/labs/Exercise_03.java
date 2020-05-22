6592616f7fbeb672aca9a5e75e812eeff949a3d1
Amethyste <ameest95@gmail.com>
2020-05-22 20:52:40 +0700

    Exercise 03

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,4 +1,7 @@
 package labs_examples.input_output.labs;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+
+import java.io.*;
 
 /**
  * Input/Output Exercise 3: variety
@@ -9,3 +12,17 @@ package labs_examples.input_output.labs;
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
  */
+
+class Variety {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        String fileName = "src/labs_examples/input_output/files/char_data.txt";
+        try {
+            fis = new FileInputStream(fileName);
+            fos = new FileOutputStream(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
