5f92488fe637bf6f6847a3776a6c8d3c346dd539
Amethyste <ameest95@gmail.com>
2020-06-01 20:02:21 +0700

    Exercise 05

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -1,4 +1,5 @@
 package labs_examples.multi_threading.labs;
+import java.util.*;
 
 /**
  * Multithreading Exercise 5:
@@ -6,3 +7,24 @@ package labs_examples.multi_threading.labs;
  *      Demonstrate the use of a wait() and notify()
  */
 
+class myThread0 implements Runnable {
+    Thread thread;
+    boolean isReady = false;
+    @Override
+    public synchronized void run() {
+        while (isReady) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isReady = true;
+        notify();
+    }
+    public synchronized static void main(String[] args) {
+        myThread0 myThread = new myThread0();
+        myThread.run();
+    }
+
+}
