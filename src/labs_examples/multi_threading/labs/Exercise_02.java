1b69bb44d8019adeeb409c3a4f8018c8c598224f
Amethyste <ameest95@gmail.com>
2020-06-01 20:01:52 +0700

    Exercise 02

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,18 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class MyThread extends Thread {
+    public void run(String name) {
+        System.out.println("I am a happy " + name);
+    }
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        MyThread myThread1 = new MyThread();
+        myThread.run("alien");
+        //set priority from Exercise 3
+        myThread1.setPriority(5);
+        myThread1.run("robot");
+    }
+
+}
