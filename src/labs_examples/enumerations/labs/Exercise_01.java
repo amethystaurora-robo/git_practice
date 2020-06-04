cbdbad564d61605ec023c153f4cb2255e5d1ebcd
Amethyste <ameest95@gmail.com>
2020-06-04 17:29:55 +0700

    enum practice

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.enumerations.labs;
 
+import com.sun.deploy.xml.GeneralEntity;
+
 /**
  * Enumerations Exercise 1:
  *
@@ -7,4 +9,32 @@ package labs_examples.enumerations.labs;
  *      2) Create a parameterized enumeration that passes one or more values to it's constructor. Demonstrate the use
  *      of this enum from a seperate class.
  */
+
+enum RichardDawkinsBooks {
+    THEBLINDWATCHMAKER("NA"),
+    THESELFISHGENE("8"),
+    THEMAGICOFREALITY("5"),
+    THEGREATESTSHOWONEARTH("10"),
+    THEANCESTORSTALE("10"),
+    THEEXTENDEDPHENOTYPE("NA"),
+    RIVEROUTOFEDEN("NA"),
+    THEDEVILSCHAPLAIN("NA"),
+    CLIMBINGMOUNTIMPROBABLE("NA");
+
+    private final String rank;
+    RichardDawkinsBooks(String rank) {
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+}
+
+class GetRanking {
+    public static void main(String[] args) {
+        RichardDawkinsBooks richardDawkinsBooks = RichardDawkinsBooks.CLIMBINGMOUNTIMPROBABLE;
+        System.out.println(richardDawkinsBooks);
+    }
+}
 
