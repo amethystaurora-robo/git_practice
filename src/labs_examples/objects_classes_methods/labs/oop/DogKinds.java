1bd756963a410bf06f99484e5eca1a57cabd49b1
Amethyste <ameest95@gmail.com>
2020-06-07 16:23:54 +0700

    New App

---
+++ src/labs_examples/objects_classes_methods/labs/oop/DogKinds.java
@@ -0,0 +1,12 @@
+package labs_examples.objects_classes_methods.labs.oop;
+
+public class DogKinds {
+    public static void main(String[] args) {
+        String[] dogArray = {"bulldog", "pug", "pitbull", "pekingnes", "bull terrier", "pomeranian", "poodle", "lab",
+        "retriever", "australian shepherd", "german shepherd", "weimeraner", "whippet", "husky", "samoyed", "alaskan malamute",
+        "irish setter", "cocker spaniel", "shi-tzu", "keeshond", "corgi", "collie", "border collie", "beagle", "rottweiler",
+        "boxer", "Old English sheepdog", "German shorthaired pointer", "basset hound", "bloodhound", "basenji", "Chinese crested",
+        "west highland white terrier", "yorkshire terrier", "springer spaniel", "french bulldog", "pharaoh hound", "tibetan mastiff",
+        "great dane", "brittany", "chihuahua", "maltese", "shar-pei", "akita", "shiba inu"};
+    }
+}
