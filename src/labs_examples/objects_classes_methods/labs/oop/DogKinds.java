892742d9080429e449d1c90cfa78f777354e35c7
Amethyste <ameest95@gmail.com>
2020-06-10 17:01:10 +0700

    New DogID App

--- src/labs_examples/objects_classes_methods/labs/oop/DogKinds.java
+++ src/labs_examples/objects_classes_methods/labs/oop/DogKinds.java
@@ -1,16 +1,23 @@
 package labs_examples.objects_classes_methods.labs.oop;
+import java.io.*;
 
 public class DogKinds {
     public static void main(String[] args) {
-        String[] dogArray = {"bulldog", "pug", "pitbull", "pekingnes", "bull terrier", "pomeranian", "poodle", "lab",
+        String[] dogArray = {"bulldog", "pug", "pitbull", "pekingese", "bull terrier", "pomeranian", "poodle", "lab",
         "retriever", "australian shepherd", "german shepherd", "weimeraner", "whippet", "husky", "samoyed", "alaskan malamute",
         "irish setter", "cocker spaniel", "shi-tzu", "keeshond", "corgi", "collie", "border collie", "beagle", "rottweiler",
         "boxer", "Old English sheepdog", "German shorthaired pointer", "basset hound", "bloodhound", "basenji", "Chinese crested",
         "west highland white terrier", "yorkshire terrier", "springer spaniel", "french bulldog", "pharaoh hound", "tibetan mastiff",
         "great dane", "brittany", "chihuahua", "maltese", "shar-pei", "akita", "shiba inu", "cairn terrier", "doberman", "afghan hound",
-        "miniature pinscher", "giant schnauzer", "ibizan hound", "airedale terrier", "basenji", "komodor", "maltese", "dachsund",
+        "miniature pinscher", "giant schnauzer", "ibizan hound", "airedale terrier", "komodor", "maltese", "dachsund",
         "chow chow", "cane corso", "bernese mountain dog", "bichon frise", "newfoundland", "cavalier king charles spaniel",
         "st bernard", "jack russell terrier", "sarabi dog", "havanese dog", "greyhound", "dalmatian", "staffordshire terrier",
         "papillon", "irish wolfhound", "kangal shepherd dog"};
+
+        String filePath = "src/labs_examples/objects_classes_methods/labs/oop/DogKinds";
     }
+}
+
+class DogMethods {
+
 }
