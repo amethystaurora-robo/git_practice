f6bd0da69c37213818bd2f6586d14db983190b96
Amethyste <ameest95@gmail.com>
2020-06-20 14:09:58 +0700

    Lambdas final

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.Function;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -8,3 +10,58 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+
+class MethodReferences {
+    @FunctionalInterface
+    interface ThisInterface {
+        void run();
+    };
+
+    @FunctionalInterface
+    interface UserGenerator{
+       User createUser(int dogNum, int plantNum);
+    }
+
+    public void thisMethod() {
+        System.out.println("This is a test.");}
+
+    public static void main (String[] args) {
+
+        //static method reference
+        Function<Integer, Integer> returnInt = (myInt) -> {
+            return myInt;
+        };
+        int b = returnInt.apply(5);
+        System.out.println(b);
+
+        //instance method reference
+        MethodReferences methodReferences = new MethodReferences();
+        ThisInterface thisInterface = methodReferences::thisMethod;
+        thisInterface.run();
+
+        //constructor reference
+        UserGenerator userGenerator = User :: new;
+        User me = userGenerator.createUser(2, 50);
+        me.toString();
+
+    }
+
+}
+
+class User{
+    int dogNum;
+    int plantNum;
+
+    public User(int dogNum, int plantNum){
+        this.dogNum = dogNum;
+        this.plantNum = plantNum;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "dogNum='" + dogNum + '\'' +
+                ", plantNum='" + plantNum + '\'' +
+                '}';
+    }
+}
