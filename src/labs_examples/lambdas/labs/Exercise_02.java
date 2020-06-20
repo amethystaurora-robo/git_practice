a1d5b72668b6a8282fa632a4fc31c5b9a8894e76
Amethyste <ameest95@gmail.com>
2020-06-20 13:47:11 +0700

    Lambdas Numero Dos

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,4 +1,5 @@
 package labs_examples.lambdas.labs;
+import java.util.function.*;
 
 /**
  * Lambdas Exercise 2:
@@ -7,3 +8,46 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+class FunctionalInterfaces {
+    public static void main(String[] args) {
+        demonstrateInterfaces();
+
+    }
+
+    public static void demonstrateInterfaces()
+
+    {
+        Function<Integer, Integer> returnInt = (myInt) -> {
+            return myInt;
+        };
+        BiFunction<Integer, Integer, Integer> returnSum = (myInt, yourInt) -> {
+            return myInt + yourInt;
+        };
+        Predicate<Boolean> returnTrue = (amSad) -> {
+            return true;
+        };
+        UnaryOperator<String> returnString = (myString) -> {
+            return myString;
+        };
+        BinaryOperator<Integer> returnDifference = (val1, val2) -> {
+            return val1 + val2;
+        };
+        Consumer<Integer> printVal = (valOne) -> {
+            System.out.println(valOne);
+        };
+        Consumer<String> printString = (myString) -> {
+            System.out.println(myString);
+        };
+        Supplier<Integer> makeVal = () -> {
+            return new Integer(5);
+        };
+        Supplier<String> makeString = () -> {
+            return new String("Hello world");
+        };
+        BiFunction<Integer, Integer, Integer> returnTimes = (thisInt, thatInt) -> {
+            return thisInt * thatInt;
+        };
+    }
+
+}
